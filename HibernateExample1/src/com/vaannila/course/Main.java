ececa38019595e5fea3cd89f20ad955129d6cda4
zsolti <szzs83@gmail.com>
2013-03-17 19:54:57 +0100

    not maven hibernate project

---
+++ HibernateExample1/src/com/vaannila/course/Main.java
@@ -0,0 +1,101 @@
+package com.vaannila.course;
+
+import java.util.List;
+import java.util.Iterator;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.vaannila.util.HibernateUtil;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Main obj = new Main();
+		Long courseId1 = obj.saveCourse("Physics");
+		Long courseId2 = obj.saveCourse("Chemistry");
+		Long courseId3 = obj.saveCourse("Maths");
+		obj.listCourse();
+		obj.updateCourse(courseId3, "Mathematics");
+		obj.deleteCourse(courseId2);
+		obj.listCourse();
+	}
+
+	public Long saveCourse(String courseName)
+	{
+		System.out.println("aaaaaaaaaaaa");
+		Session session = HibernateUtil.getSessionFactory().openSession();
+//		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		Transaction transaction = null;
+		Long courseId = null;
+		try {
+			transaction = session.beginTransaction();
+			Course course = new Course();
+			course.setCourseName(courseName);
+			courseId = (Long) session.save(course);
+			transaction.commit();
+		} catch (HibernateException e) {
+			transaction.rollback();
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+		return courseId;
+	}
+
+	public void listCourse()
+	{
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Transaction transaction = null;
+		try {
+			transaction = session.beginTransaction();
+			List courses = session.createQuery("from Course").list();
+			for (Iterator iterator = courses.iterator(); iterator.hasNext();)
+			{
+				Course course = (Course) iterator.next();
+				System.out.println(course.getCourseName());
+			}
+			transaction.commit();
+		} catch (HibernateException e) {
+			transaction.rollback();
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+	}
+
+	public void updateCourse(Long courseId, String courseName)
+	{
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Transaction transaction = null;
+		try {
+			transaction = session.beginTransaction();
+			Course course = (Course) session.get(Course.class, courseId);
+			course.setCourseName(courseName);
+			transaction.commit();
+		} catch (HibernateException e) {
+			transaction.rollback();
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+	}
+
+	public void deleteCourse(Long courseId)
+	{
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Transaction transaction = null;
+		try {
+			transaction = session.beginTransaction();
+			Course course = (Course) session.get(Course.class, courseId);
+			session.delete(course);
+			transaction.commit();
+		} catch (HibernateException e) {
+			transaction.rollback();
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+	}
+}
